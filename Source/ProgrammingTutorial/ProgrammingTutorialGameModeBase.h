5a4f8cb9562032fd4ccc39792a295f71756a45b9
KyungwonChang <kwmnusa@naver.com>
2017-07-27 17:53:58 +0900

    Project Change [Blueprint -> C++]

---
+++ Source/ProgrammingTutorial/ProgrammingTutorialGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProgrammingTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGRAMMINGTUTORIAL_API AProgrammingTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
