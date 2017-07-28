96732d8017d64b8f29dcfc67f1c374e0ecddd542
KyungwonChang <kwmnusa@naver.com>
2017-07-28 18:25:09 +0900

    BluePrint Practice

---
+++ Source/ProgrammingTutorial/MyActor.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class PROGRAMMINGTUTORIAL_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage")
+		int32 TotalDamage;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		float DamageTimeInSeconds;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Transient, Category = "Damage")
+		float DamagePerSecond;
+
+	void PostInitProperties();
+
+	UFUNCTION(BlueprintCallable, Category="Damage")
+		void CalculateValues();
+	void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Damage")
+		void CalledFromCpp();
+	void CalledFromCpp_Implementation();
+
+};
